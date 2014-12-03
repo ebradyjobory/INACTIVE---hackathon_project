c05aebd1adf3ac9213a2d876a405751095377c93
Victor Leung <victorleungtw@gmail.com>
2014-12-04 06:46:11 +0800

    add params for username

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,12 @@
 var app = {};
 
 app.init = function(){
-  this.server = 'https://api.github.com/users/essamjoubori';
-  this.fetch();
   this.students = [];
 };
 
-app.fetch = function(){
+app.fetch = function(username){
   $.ajax({
-    url: 'https://api.github.com/users/essamjoubori',
+    url: 'https://api.github.com/users/' + username,
     type: 'GET',
     contentType: 'application/json',
     success: function(data){
@@ -24,19 +22,33 @@ app.fetch = function(){
 app.addStudent = function(){
   var student = app.students[0];
 
+  console.log(student);
+
   var name = '<p>'+student.name+'</p>';
   var email = '<p>'+student.email+'</p>';
   var blog = '<p>'+student.blog+'</p>';
   var location = '<p>'+student.location+'</p>';
   var url = '<p>'+student.url+'</p>';
+
   var followers = '<p>'+student.followers+'</p>';
   var following = '<p>'+student.following+'</p>';
   var publicRepos = '<p>'+student.public_repos+'</p>';
 
+  var image = '<img src=' + student.avatar_url +'>';
+
+  $('#studentImage').append(image);
   $('#studentInfo').append(name + email + blog + location + url);
   $('#studentMetrics').append(followers + following + publicRepos);
 };
 
 $(document).ready(function(){
   app.init();
+
+  $('.search').on('click', function(e){
+    e.preventDefault();
+
+    var $username = $('#username').val();
+
+    app.fetch($username);
+  });
 });
