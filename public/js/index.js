239e0a3730e6855dfa04519f6bfd3847064780bf
Victor Leung <victorleungtw@gmail.com>
2014-12-04 05:26:19 +0800

    append github data to the dom

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@ var app = {};
 app.init = function(){
   this.server = 'https://api.github.com/users/essamjoubori';
   this.fetch();
+  this.students = [];
 };
 
 app.fetch = function(){
@@ -11,12 +12,29 @@ app.fetch = function(){
     type: 'GET',
     contentType: 'application/json',
     success: function(data){
-      console.log(data);
+      app.students.push(data);
+      app.addStudent();
     },
     error: function(data){
       console.error('something went wrong');
     },
   });
+};
+
+app.addStudent = function(){
+  var student = app.students[0];
+
+  var name = '<p>'+student.name+'</p>';
+  var email = '<p>'+student.email+'</p>';
+  var blog = '<p>'+student.blog+'</p>';
+  var location = '<p>'+student.location+'</p>';
+  var url = '<p>'+student.url+'</p>';
+  var followers = '<p>'+student.followers+'</p>';
+  var following = '<p>'+student.following+'</p>';
+  var publicRepos = '<p>'+student.public_repos+'</p>';
+
+  $('#studentInfo').append(name + email + blog + location + url);
+  $('#studentMetrics').append(followers + following + publicRepos);
 };
 
 $(document).ready(function(){
