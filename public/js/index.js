ad9a8708ddd00baad94316a0251bb4016e14c483
Victor Leung <victorleungtw@gmail.com>
2014-12-04 09:31:13 +0800

    change layout of index page

--- public/js/index.js
+++ public/js/index.js
@@ -40,15 +40,15 @@ app.fetchUserRepo = function(username){
 app.addStudent = function(){
   var student = app.students[0];
 
-  var name = '<p>'+student.name+'</p>';
-  var email = '<p>'+student.email+'</p>';
+  var name = '<h1>'+student.name+'</h1>';
+  var email = '<a href=mailto:'+student.email+'>'+student.email+'</a>';
   var blog = '<p>'+student.blog+'</p>';
   var location = '<p>'+student.location+'</p>';
   var url = '<a href='+ student.url +'>'+student.url+'</p>';
 
-  var followers = '<p>'+student.followers+'</p>';
-  var following = '<p>'+student.following+'</p>';
-  var publicRepos = '<p>'+student.public_repos+'</p>';
+  var followers = '<p> <strong>Followers:</strong> '+student.followers+'</p>';
+  var following = '<p> <strong>Following:</strong>'+student.following+'</p>';
+  var publicRepos = '<p> <strong> Repos:</strong>'+student.public_repos+'</p>';
 
   var image = '<img src=' + student.avatar_url +'>';
 
@@ -61,7 +61,7 @@ app.addStudentRepo = function(){
   // var studentRepo = app.studentRepo[0];
 
   app.studentsRepo.forEach(function(repo){
-    var name = '<p>' + repo.name + '<p>';
+    var name = '<li><span class="label label-primary">' + repo.name + '</span></li>';
     $('#studentRepos').append(name);
   });
 };
